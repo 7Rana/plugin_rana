ef2f4ebc3b2fe3ccaaad9eac9774162535e39e09
unknown <you@example.com>
2023-08-17 18:38:20 +0300

    first commit

---
+++ lib/plugin_rana.dart
@@ -0,0 +1,22 @@
+library plugin_rana;
+
+import 'package:flutter/cupertino.dart';
+
+class Pluginrana extends StatefulWidget {
+  Pluginrana({Key key}) : super(key: key);
+  _PluginranaState createState() => _PluginranaState();
+}
+
+class _PluginranaState extends State<Pluginrana>{
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(child:Text("Rana"));
+   
+   
+ }}
+ 
+ 
+ 
+ 
+ 
